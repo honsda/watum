794c8e9517d16674a6eb14fd115919405e8d6b9a
Dharon <dharon@example.com>
2026-04-21 20:52:35 +0700

    Add font preloading and improve responsive layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import soraLatin from '@fontsource-variable/sora/files/sora-latin-wght-normal.woff2';
+	import publicSansLatin from '@fontsource-variable/public-sans/files/public-sans-latin-wght-normal.woff2';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="preload" href={soraLatin} as="font" type="font/woff2" crossorigin="anonymous" />
+	<link rel="preload" href={publicSansLatin} as="font" type="font/woff2" crossorigin="anonymous" />
+</svelte:head>
 {@render children()}
